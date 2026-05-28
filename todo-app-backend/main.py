50aa68d8dcd4fda22628cdd40b2d83f30b28bfd6
temby46 <pro100pro461@gmail.com>
2026-05-29 01:15:52 +0300

    Обновлён main.py, добавлен .gitignore

--- todo-app-backend/main.py
+++ todo-app-backend/main.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
@@ -21,18 +21,23 @@ class TaskSchema(BaseModel):
 class TaskCreateSchema(BaseModel):
     title: str
 
-tasks: list[TaskSchema] = []   
+class TaskUpdateSchema(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
+
+tasks: list[TaskSchema] = []    
 
 @app.get("/tasks")
 def read_tasks() -> list[TaskSchema]:
     return tasks
 
-@app.post("/tasks")
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
 def create_task(payload: TaskCreateSchema) -> TaskSchema:
     new_task = TaskSchema(id=str(uuid4()), title=payload.title, completed=False)
 
     tasks.append(new_task)
     return new_task
+
 
 # КНИГИ
 class BookSchema(BaseModel):
@@ -53,3 +58,72 @@ def add_book(payload: BookCreateSchema):
 
     books.append(new_book)
     return {"message":f"Книга '{payload.title}' добавлена"}
+
+# Начало Patch
+@app.patch("/tasks/{task_id}")
+def update_task(task_id: str, payload: TaskUpdateSchema):
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title:
+                task.title = payload.title
+            if payload.completed is not None:
+                task.completed = payload.completed
+
+            return task
+
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT) 
+def delete_task(task_id):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+
+#Категории
+class CategorySchema(BaseModel):
+    id: str
+    name: str
+
+class CategoryCreateSchema(BaseModel):
+    name: str
+
+class CategoryUpdateSchema(BaseModel):
+    name: str | None = None
+
+categories: list[CategorySchema] = []
+
+@app.get("/categories")
+def read_categories() -> list[CategorySchema]:
+    return categories
+
+@app.post("/categories", status_code=status.HTTP_201_CREATED)
+def create_category(payload: CategoryCreateSchema) -> CategorySchema:
+    new_category = CategorySchema(id=str(uuid4()), name=payload.name)
+
+    categories.append(new_category)
+    return new_category
+
+
+@app.patch("/categories/{category_id}")
+def update_category(category_id: str, payload: CategoryUpdateSchema):
+    for category in categories:
+        if category.id == category_id:
+            if payload.name is not None:
+                category.name = payload.name
+
+            return category
+     
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Category not found"
+        )
+        
+@app.delete("/categories/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_category(category_id: str):
+    for category in categories:
+        if category.id == category_id:
+            categories.remove(category) 
+            return
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Category not found"
+        )
